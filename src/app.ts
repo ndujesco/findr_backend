c654cef1a76277a6d9996eca13da94c6a27f9bfd
ugo <ndujesco@users.noreply.github.com>
2023-10-12 15:31:15 +0100

    modify cors

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,8 @@ import { ErrorHandler } from './middleware/error';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: '*', methods: ['GET', 'POST'], allowedHeaders: '*' }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
