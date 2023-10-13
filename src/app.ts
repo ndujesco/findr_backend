49c4851e2c7c543735453e01bc59b095d1e3b168
ugo <ndujesco@users.noreply.github.com>
2023-10-13 05:32:50 +0100

    http://127.0.0.1:3000/ to header

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { ErrorHandler } from './middleware/error';
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:3000/");
     res.setHeader(
       "Access-Control-Allow-Methods",
       "OPTIONS, GET, POST, PUT, PATCH, DELETE"
