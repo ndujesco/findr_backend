07b67fccf3ea6a99e9a9a39d704db2543e686c8d
ugo <ndujesco@users.noreply.github.com>
2023-10-13 06:35:49 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -7,18 +7,18 @@ import { ErrorHandler } from './middleware/error';
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000/');
-  res.header(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-  );
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, apikey'
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000/');
+//   res.header(
+//     'Access-Control-Allow-Methods',
+//     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+//   );
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Content-Type, Authorization, apikey'
+//   );
+//   next();
+// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
