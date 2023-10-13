0683da26f8e4f4986a4d7ade59a291506d6094df
ugo <ndujesco@users.noreply.github.com>
2023-10-13 07:27:26 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-// import cors from 'cors';
+import cors from 'cors';
 
 import router from './routes';
 import { authorized } from './middleware/auth';
@@ -7,18 +7,7 @@ import { ErrorHandler } from './middleware/error';
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-  );
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, apikey'
-  );
-  next();
-});
+app.use('*', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
