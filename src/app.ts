b17285bfa99accab52b5e7e320f6e2848c10156b
ugo <ndujesco@users.noreply.github.com>
2023-10-13 05:38:26 +0100

    setHeader to header

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,12 @@ import { ErrorHandler } from './middleware/error';
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:3000/");
-    res.setHeader(
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000/");
+    res.header(
       "Access-Control-Allow-Methods",
       "OPTIONS, GET, POST, PUT, PATCH, DELETE"
     );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, apikey");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, apikey");
     next();
   });
 
