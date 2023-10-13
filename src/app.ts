317530714105affe23a4921378aa0672e98334c5
ugo <ndujesco@users.noreply.github.com>
2023-10-13 04:50:38 +0100

    refactor cors again

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,15 @@ import { ErrorHandler } from './middleware/error';
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: true }));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
