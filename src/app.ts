16998fd6ae4dbe24c9a404a838b72ab38dea0ebe
ugo <ndujesco@users.noreply.github.com>
2023-10-13 04:59:53 +0100

    refactor headers

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,11 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Methods',
     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, apikey');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader(
+    'Access-Control-Expose-Headers',
+    'Content-Type, Authorization, apikey'
+  );
   next();
 });
 
