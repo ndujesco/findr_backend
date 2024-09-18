231eb9b7fb84320746ef5683fa28cf4bce044615
ugo <ndujesco@users.noreply.github.com>
2024-09-18 18:52:48 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import { ErrorHandler } from './middleware/error';
 
 const app = express();
 
-app.use('*', cors());
+app.use('*', cors({ credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
